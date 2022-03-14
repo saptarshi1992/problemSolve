780caf42d6b719f10fcc3fbfbf62e789d83a6826
saptarshi1992@34 <saptarshi34.sc@gmail.com>
2022-03-14 15:46:02 +0530

    Update trapWater.php

--- trapWater.php
+++ trapWater.php
@@ -1,56 +1,36 @@
 <?php
+
 function trap($a)
 {
-    $maxL = [];
-    $maxR = [];
-    //$maxLdata = $a[0];
-  for($i=0;$i<count($a);$i++)
-  {
-    if($i == 0){
-        $maxL[$i] = 0;
-        $maxLdata = 0;
-    }
-    else
-    {
-        if($a[$i-1]>$maxLdata)
-        {
-            $maxLdata = $a[$i-1];
-            $maxL[$i] = $maxLdata;
-        }
-        else
-        {
-            $maxL[$i] = $maxLdata;
-        }
-    }
-
-
-  }
-  for($i=count($a)-1;$i<=0;$i--)
-  {
     
-    if($i == count($a)-1)
-    {
-        $maxRdata = 0;
-        $maxR[$j] = 0;
-    }
-    else
-    {
-        if($maxRdata<$a[$i+1])
-        {
-            $maxRdata = $a[$i+1];
-            $maxR[$j] = $maxRdata;
-        }
-        else
-        {
-            $maxR[$j] = $maxRdata;
-        }
-    }
-
-
-  }
-  print_r($maxR);
+     $water = 0;
+ 
+    // Fill left array
+    $left[0] = $a[0];
+    for ($i = 1; $i < count($a); $i++)
+    $left[$i] = max($left[$i - 1],
+                        $a[$i]);
+ 
+    // Fill right array
+    $right[count($a)- 1] = $a[count($a) - 1];
+    for ($i = count($a) - 2; $i >= 0; $i--)
+    $right[$i] = max($right[$i + 1],
+                           $a[$i]);
+ 
+    // right[i]) - arr[i] .
+    for ($i = 0; $i < count($a); $i++)
+    $water += min($left[$i], $right[$i])
+                             - $a[$i];
+ 
+    return $water;
   
 }
-//$A = [0, 1, 0, 2];
-$A = [4,2,0,3,2,5];
-trap($A);
+    // Driver program
+    $a= array(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1);
+    
+    echo trap($a);
+
+
+
+ ?>
+
